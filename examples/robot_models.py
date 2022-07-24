237d791da63727ceb739130a5ed437f6a059cde1
Tomas Lozano-Perez <tlp@mit.edu>
2022-07-24 10:33:16 +0200

    Panda

--- examples/robot_models.py
+++ examples/robot_models.py
@@ -4,7 +4,7 @@ import time
 
 import numpy as np
 
-import skrobot_tlp as skrobot
+import skrobot as skrobot
 
 import pdb
 
--- skrobot/models/panda.py
+++ skrobot/models/panda.py
@@ -1,9 +1,12 @@
 from cached_property import cached_property
+import numpy as np
+import trimesh
 
-from ..data import panda_urdfpath
-from ..model import RobotModel
+from skrobot.coordinates import CascadedCoords
+from skrobot.data import panda_urdfpath
+from skrobot.model import RobotModel
+
 from .urdf import RobotModelFromURDF
-
 
 class Panda(RobotModelFromURDF):
 
@@ -14,13 +17,68 @@ class Panda(RobotModelFromURDF):
 
     def __init__(self, *args, **kwargs):
         super(Panda, self).__init__(*args, **kwargs)
-        self.reset_pose()
+
+        # Tool
+        self.rarm_end_coords = CascadedCoords(
+            parent=self.panda_hand,
+            name='rarm_end_coords')
+        self.rarm_end_coords.translate(
+            np.array([0.0, 0.0, 0.1], dtype=np.float32))
+        self.rarm_end_coords.rotate(- np.pi , axis='x')
+        # Wrist
+        self.rarm_wrist_coords = CascadedCoords(
+            parent=self.panda_link8,
+            name='rarm_wrist_coords')
+        self.chains = set(['base', 'right'])
+        self.chain_type = {'base': 'fixed',
+                           'right': 'revolute_chain'}
+        self.base_link = self.panda_link0
+        self.base_links = \
+            [self.panda_link0]
+        self.link_lists = \
+            {'right': self.rarm.link_list,
+             'base': self.base_links}
+        self.joint_lists = \
+            {chain: [link.joint for link in self.link_lists[chain]] for chain \
+             in self.chains}
+        self.end_coords = \
+            {'right': self.rarm_end_coords}
+        self.hand_links = \
+            {'right': [self.panda_hand,
+                       self.panda_rightfinger,
+                       self.panda_leftfinger]}
+        self.collision_link_lists = \
+            {'right': self.link_lists['right'] + self.hand_links['right'],
+             'base': self.link_lists['base']}
+        self.hand_body_names = \
+            {h : set([l.name for l in self.hand_links[h]]) \
+             for h in ('right',)}
+        self.base_body_names = set([l.name for l in self.base_links])
+        self.arm_body_names = \
+            {h : set([l.name for l in set(self.collision_link_lists[h]) - set(self.hand_links[h])]) \
+             for h in ('right',)}
+        # To enable GJK collision checking
+        for chain in self.collision_link_lists:
+            for link in self.collision_link_lists[chain]:
+                link.collision_mesh.convex_mesh = trimesh.convex.convex_hull(link.collision_mesh)
+                link.collision_mesh.convex_mesh_vertices = \
+                    np.ascontiguousarray(link.collision_mesh.convex_mesh.vertices, dtype=np.double)
+        # Self collision links
+        self.self_collision_link_name_pairs = set()
+        for link1 in self.collision_link_lists['right']:
+            for link2 in self.collision_link_lists['base']:
+                if link1 in (self.panda_link1,) \
+                   and link2 in self.base_links:
+                    continue
+                self.self_collision_link_name_pairs.add(tuple(sorted((link1.name, link2.name))))
+        
+        self._reset_manip_pose()
 
     @cached_property
     def default_urdf_path(self):
         return panda_urdfpath()
 
-    def reset_pose(self):
+    def _reset_manip_pose(self):
         angle_vector = [
             0.03942226991057396,
             -0.9558116793632507,
@@ -34,6 +92,8 @@ class Panda(RobotModelFromURDF):
             link.joint.joint_angle(angle)
         return self.angle_vector()
 
+    reset_manip_pose = _reset_manip_pose
+
     @cached_property
     def rarm(self):
         link_names = ['panda_link{}'.format(i) for i in range(1, 8)]
@@ -42,3 +102,26 @@ class Panda(RobotModelFromURDF):
         model = RobotModel(link_list=links, joint_list=joints)
         model.end_coords = self.panda_hand
         return model
+
+    def gripper_distance(self, dist=None, arm='right'):
+        """Change gripper angle function
+
+        Parameters
+        ----------
+        dist : None or float
+            gripper distance.
+            If dist is None, return gripper distance.
+            If flaot value is given, change joint angle.
+        arm : str
+            Specify target arm.  You can only specify 'right'
+
+        Returns
+        -------
+        dist : float
+            Result of gripper distance in meter.
+        """
+        if dist is not None:
+            self.panda_finger_joint1.joint_angle(dist/2)
+            self.panda_finger_joint2.joint_angle(dist/2)
+        return self.panda_finger_joint1.joint_angle() + self.panda_finger_joint2.joint_angle()
+            
